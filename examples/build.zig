a4b1543db67df6f2b99f87c389bde7cee33969ba
Joel <silverclaw@noreply.codeberg.org>
2025-03-11 22:00:51 +0100

    move Umka scripts into src

--- examples/build.zig
+++ examples/build.zig
@@ -27,6 +27,7 @@ pub fn build(b: *std.Build) void {
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
+    run_cmd.cwd = b.path("src");
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
